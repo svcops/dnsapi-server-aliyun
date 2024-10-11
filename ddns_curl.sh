3183f9f786c2594467c828a0b81f9ebfdf4437d0
zhenjie zhu <tech@intellij.io>
2024-10-12 04:43:47 +0800

    Add restart policy to Docker and refine logging script
    Added a restart policy to the Docker service in docker-compose.yml to ensure the service always restarts on failure. Simplified the log function in ddns_curl.sh by removing redundant default value checks.

--- ddns_curl.sh
+++ ddns_curl.sh
@@ -3,12 +3,6 @@
 function log() {
   local remark="$1"
   local msg="$2"
-  if [ -z "$remark" ]; then
-    remark="unknown remark"
-  fi
-  if [ -z "$msg" ]; then
-    msg="unknown message"
-  fi
   local now=$(date +"%Y-%m-%d %H:%M:%S")
   echo -e "$now - [ $remark ] $msg"
 }
