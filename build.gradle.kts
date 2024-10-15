8590790374e8856d2a6984f2776845b94286c7ec
zhenjie zhu <tech@intellij.io>
2024-10-15 12:14:41 +0800

    Switch to default web server & disable Spring banner
    Commented out Jetty starter in favor of default Tomcat in the build configuration. Added configuration to disable the Spring banner in the application properties file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,11 +32,12 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web") {
-        exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat")
-    }
+    // implementation("org.springframework.boot:spring-boot-starter-web") {
+    //     exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat")
+    // }
 
-    implementation("org.springframework.boot:spring-boot-starter-jetty")
+    // implementation("org.springframework.boot:spring-boot-starter-jetty")
+    implementation("org.springframework.boot:spring-boot-starter-web")
 
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
